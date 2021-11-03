4c3c7902b956d4104a8935ddd81b41334b7a96f3
Levi Durfee <levi.durfee@gmail.com>
2021-11-03 16:13:15 -0400

    Merge dev (#1)
    * Tidy up
    
    * Change JSON structure
    
    * Update docs

--- README.md
+++ README.md
@@ -1,3 +1,68 @@
 # urlchecker
 
-* [Available Environment Variables](https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables)
+Below is an example YAML file for this action.
+
+```yaml
+name: Check URLs
+
+on:
+  push:
+    branches:
+      - '*'
+
+env:
+  SLACK_WEBHOOK: ${{ secrets.SLACK_WEBHOOK }}
+
+jobs:
+  check-urls:
+    runs-on: ubuntu-latest
+    name: Checks URLs from JSON file
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+      - name: Check URLs
+        uses: bythepixel/urlchecker
+        args:
+          - ./urls.json
+```
+
+## Description
+
+This GitHub Action reads a JSON file, crawls the URLs, and checks the resposnes.
+
+## Requirements
+
+* A `SLACK_WEBHOOK` URL to send a message when something goes wrong
+* A JSON file of URLs in your repository that uses the following structure
+
+## JSON File
+
+```json
+[
+    {
+        "url": "https://postman-echo.com/status/200",
+        "status": 200
+    },
+    {
+        "url": "https://postman-echo.com/status/200",
+        "status": 200
+    },
+    {
+        "url": "https://postman-echo.com/status/200",
+        "status": 200,
+        "regex": "200"
+    }
+]
+```
+
+View the files in the [json](json) folder to see more examples. See the Golang
+[regexp][1] package for additional information on supported regular expressions.
+
+## Environment Variables
+
+This Action uses these environment variables
+
+* `SLACK_WEBHOOK` is one you need to provide
+* `GITHUB_REPOSITORY` is provided by GitHub
+
+[1]: https://pkg.go.dev/regexp
--- main.go
+++ main.go
@@ -9,56 +9,81 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"regexp"
 )
 
-const EnvSlackWebhook = "SLACK_WEBHOOK"
+const (
+	EnvSlackWebhook = "SLACK_WEBHOOK"
+	EnvGithubRepo   = "GITHUB_REPOSITORY"
+)
 
-type payload struct {
+type HealthCheck struct {
+	// URL to check
+	URL string `json:"url"`
+
+	// Status code expected from URL
+	Status int `json:"status"`
+
+	// Regex used to check the body of the response
+	Regex string `json:"regex"`
+}
+
+// SlackWebhookPayload represents the minimum required fields to send a webhook.
+//
+// https://api.slack.com/messaging/webhooks
+type SlackWebhookPayload struct {
 	Text string `json:"text"`
 }
 
+// SlackClient contains the Webhook URL.
 type SlackClient struct {
 	Webhook string
 }
 
-func (c SlackClient) SendMessage(status int, message string) {
-	repo := os.Getenv("GITHUB_REPOSITORY")
-	msg := fmt.Sprintf("Repository: %s URL: %s responded with %d", repo, message, status)
+// SendMessage creates a SlackWebhookPayload and sends it to the Webhook URL.
+func (c SlackClient) SendMessage(status int, url string, message string) {
+	repo := os.Getenv(EnvGithubRepo)
+	msg := fmt.Sprintf("Repository: %s URL: %s Message: %s", repo, url, message)
 
-	pl := payload{
+	pl := SlackWebhookPayload{
 		Text: msg,
 	}
 
 	jsonPayload, _ := json.Marshal(pl)
 
-	_, err := http.Post(c.Webhook, "application/json",
-		bytes.NewBuffer(jsonPayload))
+	_, err := http.Post(c.Webhook, "application/json", bytes.NewBuffer(jsonPayload))
 
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func fetch(url string) (int, error) {
+func fetch(url string) (int, string, error) {
 	resp, err := http.Get(url)
-
 	if err != nil {
-		return 0, err
+		return 0, "", err
 	}
-
 	defer resp.Body.Close()
 
-	return resp.StatusCode, nil
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, "", err
+	}
+
+	return resp.StatusCode, string(body), nil
 }
 
 func main() {
-	fmt.Println("::debug::Starting...")
-
+	// A Slack Webhook must be specified as an environment variable.
 	webhook := os.Getenv(EnvSlackWebhook)
 	if webhook == "" {
 		log.Fatalf("Missing '%s' Environment Variable", EnvSlackWebhook)
 	}
+	slack := SlackClient{
+		Webhook: webhook,
+	}
 
+	// A filename must be specified for the program to read.
 	var filename string
 	flag.StringVar(&filename, "filename", "", "JSON File with URLs")
 	flag.Parse()
@@ -66,31 +91,52 @@ func main() {
 		log.Fatal("Missing filename flag")
 	}
 
-	slack := SlackClient{
-		Webhook: webhook,
-	}
-
+	// Attempt to read the file specified.
 	log.Printf("Reading %s...\n", filename)
-
-	bytes, _ := ioutil.ReadFile(filename)
-
-	var urls []string
-	err := json.Unmarshal(bytes, &urls)
+	bytes, err := ioutil.ReadFile(filename)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	for _, url := range urls {
-		log.Printf("Checking %s...\n", url)
-		status, err := fetch(url)
+	// Attempt to parse the file content as JSON.
+	var urls []HealthCheck
+	err = json.Unmarshal(bytes, &urls)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	// Loop through URLs and check each one.
+	for _, check := range urls {
+		log.Printf("Checking %s...\n", check.URL)
+
+		status, body, err := fetch(check.URL)
 		if err != nil {
+			// Log the error and keep going.
 			log.Printf("Error: %s\n", err.Error())
 		}
 
-		if status != 200 {
-			log.Printf("Invalid HTTP Response Status %d\n", status)
-			slack.SendMessage(status, url)
+		if status != check.Status {
+			// Log the invalid response, send it to slack, then move onto the
+			// next URL. We want to crawl every URL, so we don't exit if a URL
+			// returns an incorrect response.
+			msg := fmt.Sprintf("Invalid HTTP Response Status %d", status)
+			slack.SendMessage(status, check.URL, msg)
 			continue
+		}
+
+		if check.Regex != "" {
+			log.Println("Checking regex")
+			re, err := regexp.Compile(check.Regex)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			matches := re.MatchString(body)
+			if !matches {
+				log.Println("HTTP Response Body Error")
+				slack.SendMessage(status, check.URL, "HTTP Response Body Error")
+				continue
+			}
 		}
 
 		log.Println("Good")
