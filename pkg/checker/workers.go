b56b67650b0864d7cf77d42b7703e96371a6a964
Levi Durfee <levi.durfee@gmail.com>
2022-03-22 14:24:32 -0400

    Remove commented out prints

--- pkg/checker/workers.go
+++ pkg/checker/workers.go
@@ -18,7 +18,7 @@ func XMLWorker(ctx context.Context, urlChan chan string, id int, messager Messag
 			if !ok {
 				return
 			}
-			// log.Printf("Worker [%d] %s...\n", id, url)
+
 			status, _, err := client.Fetch(url)
 			if err != nil {
 				log.Printf("Error: %s\n", err.Error())
@@ -31,8 +31,6 @@ func XMLWorker(ctx context.Context, urlChan chan string, id int, messager Messag
 			}
 
 			time.Sleep(sleep)
-
-			// log.Printf("Worker [%d] %s good!\n", id, url)
 		case <-ctx.Done():
 			return
 		}
